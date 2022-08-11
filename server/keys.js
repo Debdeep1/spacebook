77ee277033e8f91dd5d9c0c789b592c4097d30e3
Debdeep1 <deb.baner.1@gmail.com>
2022-08-11 18:31:15 +0530

    JWT Auth

--- server/keys.js
+++ server/keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    MONGOURL:"mongodb+srv://spacebook:spacebook@spacebookdb.getiyxe.mongodb.net/?retryWrites=true&w=majority"
-
+    MONGOURL:"mongodb+srv://spacebook:spacebook@spacebookdb.getiyxe.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"asuhd122jnjd43758234ibdaih"
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken')
-
+const {JWT_SECRET} = require('../keys')
 
 router.post("/signup", (req, res) => {
   const { name, email, password } = req.body;
@@ -50,7 +50,9 @@ router.post('/signin',(req,res)=>{
         }
         bcrypt.compare(password,savedUser.password).then(doMatch=>{
             if(doMatch){
-                res.json({message:"successful sign in"})
+                //res.json({message:"successful sign in"})
+                const token = jwt.sign({_id:savedUser._id}, JWT_SECRET)
+                res.json({token})
             }
             else{
                 return res.status(422).json({error:"Invalid credentials"})
