a804376f3edf4d50a187077dec2e49cc08ae3264
Debdeep1 <deb.baner.1@gmail.com>
2022-08-11 21:06:10 +0530

    ...

--- server/middleware/requireLogin.js
+++ server/middleware/requireLogin.js
@@ -5,7 +5,7 @@ const User = mongoose.model("User")
 
 module.exports = (req,res,next)=>{
     const {authorization} = req.headers
-    //authorization === Bearer ewefwegwrherhe
+    
     if(!authorization){
        return res.status(401).json({error:"you must be logged in"})
     }
