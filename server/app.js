dc27e8345d57364a468d201613c697e149c7f35a
Debdeep1 <deb.baner.1@gmail.com>
2022-08-11 12:44:05 +0530

    setting up server

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express =  require('express')
+const app = express()
+const mongoose = require('mongoose')
+const PORT = 5000
+const {MONGOURL}= require('./keys')
+
+
+require('./models/user')
+
+mongoose.connect(MONGOURL,{
+    useNewUrlParser:true, useUnifiedTopology:true
+})
+mongoose.connection.on('connected',()=>{
+    console.log('Connected to mongo...')
+})
+mongoose.connection.on('error',(err)=>{
+    console.log('error connecting...',err)
+})
+
+app.listen(PORT,()=>{
+    console.log("Spacebook is launching on", PORT)
+})
---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURL:"mongodb+srv://spacebook:spacebook@spacebookdb.getiyxe.mongodb.net/?retryWrites=true&w=majority"
+
+}
