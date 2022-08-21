997502ed61d4c05d7425e59cc9e1a149afc4ef81
Debdeep1 <deb.baner.1@gmail.com>
2022-08-21 09:56:39 +0530

    routes done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import NavbarCompo from "./components/NavbarComponent";
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <NavbarCompo/>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
       </Routes>
     </>
   );
