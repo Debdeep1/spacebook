1fe9008abe1610f839b8fefb77dbdda60a1086e9
Debdeep1 <deb.baner.1@gmail.com>
2022-08-27 19:29:30 +0530

    bas r na

--- server/keys.js
+++ server/keys.js
@@ -1,7 +1,4 @@
 module.exports={
     MONGOURL:"mongodb+srv://spacebook:spacebook@spacebookdb.getiyxe.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET:"asuhd122jnjd43758234ibdaih",
-    CLOUD_NAME: "spacebook9",
-CLOUD_KEY: "599127717631735",
-CLOUD_KEY_SECRET: "RfpXDIt_cGtb6O5tY7a2xv33K5I"
+    JWT_SECRET:"asuhd122jnjd43758234ibdaih"
 }
