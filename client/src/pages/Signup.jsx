b45edc1784506c10ff589e76935d7a2af2b94dbe
Debdeep1 <deb.baner.1@gmail.com>
2022-08-27 07:41:44 +0530

    buttons

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,19 +1,36 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Nav from 'react-bootstrap/Nav';
 import "./styles/signup.css"
 function Signup() {
+
+  const [name,setName]= useState("")
+  const [password,setPassword]= useState("")
+  const [email,setEmail]= useState("")
+  const PostData =()=>{
+    fetch("http://localhost:5000/signup",{
+      method:"post",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify({
+        name:"",
+        password:"",
+        email:"",
+      })
+    }).then(res=>res.json()).then(data=>{console.log(data)})
+  }
+  //5:51
   return (
     <Card>
       <Card.Header>
         <Nav variant="pills" defaultActiveKey="#signup">
           <Nav.Item>
-            <Nav.Link href="#signup">Signup</Nav.Link>
+            <a target="_blank" href="#signup">Signup</a>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#disabled" disabled>
+            <a target="_blank" href="#disabled" disabled>
               Disabled
-            </Nav.Link>
+            </a>
           </Nav.Item>
         </Nav>
       </Card.Header>
@@ -23,8 +40,9 @@ function Signup() {
           Welcome Traveller!
         </Card.Text>
         <Card.Text>
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+        <input type="text" placeholder="User Name" value={name} onChange={(e)=>setName(e.target.value)} />
+          <input type="text" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+          <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
         </Card.Text>
         <Button href="/login" variant="primary">Already have an account?</Button>
       </Card.Body>
