6c177f9f57bed5b0735ac1197837ff9444f5e773
Debdeep1 <deb.baner.1@gmail.com>
2022-08-28 11:26:28 +0530

    yayy

--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -3,8 +3,10 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import "./styles/navbar.css"
+import {useNavigate} from 'react-router-dom'
 
 function NavbarCompo() {
+  const navigate = useNavigate()
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
@@ -15,13 +17,15 @@ function NavbarCompo() {
             <Nav.Link className='NavItemLinks' href="/">Home</Nav.Link>
             <Nav.Link className='NavItemLinks' href="/profile">Profile</Nav.Link>
             <NavDropdown title="Action" id="basic-nav-dropdown">
+            <NavDropdown.Item className='NavItemLinks' href="/create">
+                Create Post
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item className='NavItemLinks' onClick={()=>{
+                localStorage.clear(); navigate(`/login`) }}>LogOut</NavDropdown.Item>
               <NavDropdown.Item className='NavItemLinks' href="/login">Login</NavDropdown.Item>
               <NavDropdown.Item className='NavItemLinks' href="/signup">
                 Sign Up
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item className='NavItemLinks' href="/create">
-                Create Post
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavbarCompo from "../components/NavbarComponent";
 import "./styles/profile.css";
 
 const Profile = () => {
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("/allpost", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result.posts);
+      });
+  }, []);
+
   return (
     <>
       <NavbarCompo />
+      {data.map((item)=>{
+        return (
+          <>
       <div className="ProfileContainer">
         <div className="ProImgContainer">
           <img
@@ -14,7 +31,7 @@ const Profile = () => {
           />
         </div>
         <div className="ProInfoContainer">
-          <h4>Doggo The Boy</h4>
+          <h4>{item.postedBy.name}</h4>
           <div className="Info">
             <h6>10 posts</h6>
             <h6>10 followers</h6>
@@ -23,13 +40,11 @@ const Profile = () => {
         </div>
       </div>
       <div className="ProGalleryContainer">
-        <img className="galleryItem" src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg" />
-        <img className="galleryItem" src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg" />
-        <img className="galleryItem" src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg" />
-        <img className="galleryItem" src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg" />
-        <img className="galleryItem" src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg" />
-        <img className="galleryItem" src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg" />
+          <img className="galleryItem" src={item.url} />
       </div>
+      </>
+        )
+      })}
     </>
   );
 };
